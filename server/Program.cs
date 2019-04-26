c1c74ab07a0e1ad27e8294c92b122724ec62288d
Ruben Diaz <ruben.diaz@metrica.es>
2019-04-26 14:52:10 +0200

    Multithreading

--- server/Program.cs
+++ server/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
+
+using System.Threading;
 
 namespace server
 {
@@ -20,74 +24,48 @@ namespace server
 
         class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-            TcpListener server = null;
-            try
+            Program main = new Program();
+            main.server_start();  //starting the server
+
+            Console.ReadLine();
+        }
+
+        TcpListener server = new TcpListener(Config.Adress, Config.Port);
+
+        private void server_start()
+        {
+            server.Start();
+            accept_connection();  //accepts incoming connections
+        }
+
+        private void accept_connection()
+        {
+            server.BeginAcceptTcpClient(handle_connection, server);  //this is called asynchronously and will run in a different thread
+        }
+
+        private void handle_connection(IAsyncResult result)  //the parameter is a delegate, used to communicate between threads
+        {
+            accept_connection();  //once again, checking for any other incoming connections
+            TcpClient client = server.EndAcceptTcpClient(result);  //creates the TcpClient
+
+            NetworkStream ns = client.GetStream();
+
+            /* here you can add the code to send/receive data */
+            int i;
+            String data = string.Empty;
+            Byte[] bytes = new Byte[8192];
+            while ((i = ns.Read(bytes, 0, bytes.Length)) != 0)
             {
-                // TcpListener server = new TcpListener(port);
-                server = new TcpListener(Config.Adress, Config.Port);
-
-                // Start listening for client requests.
-                server.Start();
-
-                // Buffer for reading data
-                Byte[] bytes = new Byte[8192];
-                String data = null;
-
-                // Enter the listening loop.
-                while (true)
-                {
-                    Console.Write("Waiting for a connection... ");
-
-                    // Perform a blocking call to accept requests.
-                    // You could also user server.AcceptSocket() here.
-                    TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("Connected!");
-
-                    data = null;
-
-                    // Get a stream object for reading and writing
-                    NetworkStream stream = client.GetStream();
-
-                    int i;
-
-                    // Loop to receive all the data sent by the client.
-                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                    {
-                        // Translate data bytes to a ASCII string.
-                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        Console.WriteLine("Received: {0}", data);
-
-                        // Process the data sent by the client.
-                        data = data.ToUpper();
-
-                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                        // Send back a response.
-                        //stream.Write(msg, 0, msg.Length);
-                        //Console.WriteLine("Sent: {0}", data);
-                        
-                    }
-                    stream.Close();
-
-                    // Shutdown and end connection
-                    client.Close();
-                }
+                // Translate data bytes to a ASCII string.
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Received: {0}", data);
             }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-            finally
-            {
-                // Stop listening for new clients.
-                server.Stop();
-            }
-
-
-            Console.WriteLine("\nHit enter to continue...");
-            Console.Read();
+            //byte[] msg = Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+            //ns.WriteAsync(msg, 0, msg.Length);
+            //Console.WriteLine("Sent: {0}", msg);
+            //client.Close();
         }
     }
 }
