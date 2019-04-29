027062e1a5e0e4e6b69226869a61fdebf5c57f30
Primc <pablocruzmendez@gmail.com>
2019-04-29 17:00:28 +0200

    description added, and new class config

---
+++ core_server/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace config
+{
+    class Config
+    {
+        public static string Adress { get; set; } = "0.0.0.0";
+        public static int Port { get; set; } = 8080;
+        public static int Wait { get; set; } = 300;
+    }
+}
